2cc87558d86e022bbae9cf53b9fcf1f8d912924f
Aun-Raza <aunrazaq123@gmail.com>
2023-07-03 13:44:39 -0400

    Delete client folder

--- client/src/components/Home.tsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable linebreak-style */
-import React from 'react';
-
-function Home() {
-  return <h2 className='m-4'>Home page</h2>;
-}
-
-export default Home;
--- client/src/components/NotFound.tsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable linebreak-style */
-import React from 'react';
-
-function NotFound() {
-  return <h2 className='m-4'>Error 404: page not found</h2>;
-}
-
-export default NotFound;
